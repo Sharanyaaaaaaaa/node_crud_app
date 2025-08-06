d465c21e35248a45176bd9dacc2d623acedbd367
Sharanyaaaaaaaa <148205524+Sharanyaaaaaaaa@users.noreply.github.com>
2025-08-06 22:52:26 +0530

    Post request
    post request api created

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,20 @@
-module.export = (req, res) => {};
+const crypto = require("crypto");
+const requestBodyparser = require("../util/body-parser");
+module.exports = async (req, res) => {
+    if(req.url === "/api/movies") {
+        try {
+            let body =  await requestBodyparser(req);
+             body.id = crypto.randomUUID();
+             req.movies.push(body);
+             res.writeHead(201,{"Content-Type":"application/json"});
+             res.end();
+            console.log("Request Body:", body);
+        } catch (err){
+            console.log(err);
+             res.writeHead(400,{"Content-Type":"application/json"});
+        
+            res.end(JSON.stringify({title:"Validation failed",message: "Request body is not valid"})
+        );
+        }
+    }
+};
